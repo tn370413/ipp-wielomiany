d354ab99f2d91052c30b431e6cdef0be86d56bfd
Tomasz Necio <Tomasz.Necio@fuw.edu.pl>
2017-05-23 16:46:37 +0200

    Started implementing changes to make the program accept infinitely long input lines

--- src/main.c
+++ src/main.c
@@ -9,6 +9,8 @@
 #define COMMAND 1
 #define POLYNOMIAL 2
 #define MAX_MONOS_COUNT 200
+#define MAX_SIZE_COMMAND 50
+#define
 
 #define UNDERFLOW_ERR "ERROR %d STACK UNDERFLOW\n"
 #define PARSING_ERR "ERROR %d %d\n"
@@ -16,6 +18,8 @@
 #define WRONG_VALUE_ERR "ERROR %d WRONG VALUE\n"
 #define WRONG_VARIABLE_ERR "ERROR %d WRONG VARIABLE\n"
 
+
+/* I/O AND STRING PROCESSING AUXILLIARY FUNCTIONS */
 
 void PrintError(char* msg, unsigned row, int column) {
 	fprintf(stderr, msg, row + 1, column + 1);
@@ -28,6 +32,106 @@ void BoolPrint(bool val) {
 		printf("0\n");
 	}
 }
+
+
+bool IsLetter(int c) {
+	if (('z' >= c && c >= 'a') || ('Z' >= c && c >= 'A')) {
+		return true;
+	}
+	return false;
+}
+
+int SeeChar() {
+	int c = getchar();
+	ungetc(c, stdin);
+	return c;
+}
+
+bool GetCharByReference();
+
+/* I/O <-> INTERNAL REPR. CONVERSION FUNCTIONS */
+
+void PolyPrint(Poly *p);
+
+void MonoPrint(Mono *m) {
+	printf("(");
+	PolyPrintAux(&(m->p));
+	printf(",%d)", m->exp);
+}
+
+void PolyPrintAux(Poly *p) {
+	if (PolyIsCoeff(p)) {
+		printf("%d", (int) p->scalar);
+	} else {
+		if (p->scalar != 0) {
+			printf("(%d,0)", (int) p->scalar);
+		}
+		for (unsigned i = 0; i < p->monos_count; i++) {
+			if (i > 0 || p->scalar != 0) {
+				printf("+");
+			}
+			MonoPrint(&(p->monos[i]));
+		}
+	}
+}
+
+void PolyPrint(Poly *p) {
+	PolyPrintAux(p);
+	printf("\n");
+}
+
+Poly PolyParseStdin(char start, unsigned row, unsigned *column);
+
+Mono MonoParse(char *in, unsigned row, unsigned *column) {
+	(*column)++;
+	Poly p = PolyParseStdin(in, column, row);
+	if ((*column)[0] != ',') {
+		PrintError(PARSING_ERR, row, (*column) - in);
+	}
+	(*column)++;
+	poly_exp_t e = strtol(*column, column, 10);
+	if ((*column)[0] != ')') {
+		PrintError(PARSING_ERR, row, (*column) - in);
+	}
+	(*column)++;
+	return MonoFromPoly(&p, e);
+}
+
+Poly PolyParseStdin(unsigned row, unsigned *column) {
+	if (SeeChar() == '(') { /* array of monos */
+		Mono monos[MAX_MONOS_COUNT]; // !! źle, ich moze byc w ch..
+		unsigned monos_count = 0;
+
+		while (true) {
+			if (monos_count == MAX_MONOS_COUNT) {
+				monos = GrowArray(&monos); // TODO
+			}
+
+			monos[monos_count] = MonoParse(in, row, column);
+			monos_count++;
+			if (() != '+') {
+				break;
+			}
+			(*column)++;
+		}
+		return PolyAddMonos(monos_count, monos);
+
+	} else /* a scalar */ {
+		char number[MAX_SIZE_INT_BASE_10]; // max no of digit for an int in base 10
+		int ch = getchar();
+		for (unsigned i = 0; IsDigit(ch); i++) {
+			number[i] = (char) ch;
+			ch = getchar();
+		}
+		ungetc(stdin); // we leave the non-digit where we found it
+		number[i + 1] = 0;
+		poly_coeff_t r = strtol(*number, NULL, 10);
+		return PolyFromCoeff(r);
+	}
+}
+
+
+/* OPERATIONS ON POLYS ON THE STACK */
 
 void ActOnTwoPolysOnStack(Stack *s, unsigned row,
 						  Poly (*op)(const Poly *, const Poly *)) {
@@ -69,142 +173,100 @@ bool AreEqualTwoPolysFromStack(Stack *s) {
 	return r;
 }
 
-void PolyPrint(Poly *p);
 
-void MonoPrint(Mono *m) {
-	printf("(");
-	PolyPrintAux(&(m->p));
-	printf(",%d)", m->exp);
-}
-
-void PolyPrintAux(Poly *p) {
-	if (PolyIsCoeff(p)) {
-		printf("%d", (int) p->scalar);
-	} else {
-		if (p->scalar != 0) {
-			printf("(%d,0)", (int) p->scalar);
-		}
-		for (unsigned i = 0; i < p->monos_count; i++) {
-			if (i > 0 || p->scalar != 0) {
-				printf("+");
-			}
-			MonoPrint(&(p->monos[i]));
-		}
-	}
-}
-
-void PolyPrint(Poly *p) {
-	PolyPrintAux(p);
-	printf("\n");
-}
-
-Poly PolyParse(char *in, char **pos, unsigned row);
-
-Mono MonoParse(char *in, char **pos, unsigned row) {
-	(*pos)++;
-	Poly p = PolyParse(in, pos, row);
-	if ((*pos)[0] != ',') {
-		PrintError(PARSING_ERR, row, (*pos) - in);
-	}
-	(*pos)++;
-	poly_exp_t e = strtol(*pos, pos, 10);
-	if ((*pos)[0] != ')') {
-		PrintError(PARSING_ERR, row, (*pos) - in);
-	}
-	(*pos)++;
-	return MonoFromPoly(&p, e);
-}
-
-Poly PolyParse(char *in, char **pos, unsigned row) {
-	if ((*pos)[0] == '(') {
-		/* array of monos */
-		Mono monos[MAX_MONOS_COUNT];
-		unsigned monos_count = 0;
-		while (true) {
-			monos[monos_count] = MonoParse(in, pos, row);
-			monos_count++;
-			if ((*pos)[0] != '+') {
-				break;
-			}
-			(*pos)++;
-		}
-		return PolyAddMonos(monos_count, monos);
-
-	} else /* a scalar */ {
-		poly_coeff_t r = strtol(*pos, pos, 10);
-		return PolyFromCoeff(r);
-	}
-}
+/* THE PROGRAM */
 
 void ExecuteCommand(Stack *s, char *command, unsigned row) {
-	char start = command[0];
-	if (('z' >= start && start >= 'a') || ('Z' >= start && start >= 'A')) {
-		if (strcmp(command, "ZERO") == 0) {
-			Poly p = PolyZero();
-			Push(s, &p);
-		} else if (strcmp(command, "IS_COEFF") == 0) {
-			Poly top = GetTop(s);
-			BoolPrint(PolyIsCoeff(&top));
-		} else if (strcmp(command, "IS_ZERO") == 0) {
-			Poly top = GetTop(s);
-			BoolPrint(PolyIsZero(&top));
-		} else if (strcmp(command, "CLONE") == 0) {
-			Poly top = GetTop(s);
-			Poly p = PolyClone(&top);
-			Push(s, &p);
-		} else if (strcmp(command, "ADD") == 0) {
-			AddTwoPolysFromStack(s, row);
-		} else if (strcmp(command, "MUL") == 0) {
-			MultiplyTwoPolysFromStack(s, row);
-		} else if (strcmp(command, "NEG") == 0) {
-			Poly p = Pop(s);
-			Poly q = PolyNeg(&p);
-			PolyDestroy(&p);
-			Push(s, &q);
-		} else if (strcmp(command, "SUB") == 0) {
-			SubtractTwoPolysFromStack(s, row);
-		} else if (strcmp(command, "IS_EQ") == 0) {
-			BoolPrint(AreEqualTwoPolysFromStack(s));
-		} else if (strcmp(command, "DEG") == 0) {
-			Poly top = GetTop(s);
-			printf("%d\n", PolyDeg(&top));
-		} else if (strcmp(command, "PRINT") == 0) {
-			Poly top = GetTop(s);
-			PolyPrint(&top);
-			printf("\n");
-		} else if (strcmp(command, "POP") == 0) {
-			Poly p = Pop(s);
-			PolyDestroy(&p);
-		} else if (strncmp(command, "DEG_BY ", 7) == 0) { // TODO ERROR MSGS
-			Poly top = GetTop(s);
-			printf("%d\n", PolyDegBy(&top, strtol(command + 7, NULL, 10)));
-		} else if (strncmp(command, "AT ", 3) == 0) { // TODO ERROR MSGS
-			Poly top = GetTop(s);
-			Poly p = PolyAt(&top, strtol(command + 3, NULL, 10));
-			PolyPrint(&p);
-		} else {
-			PrintError(WRONG_COMMAND_ERR, row, 0);
-		}
-	} else {
-		Poly p = PolyParse(command, &command, row);
+	if (strcmp(command, "ZERO") == 0) {
+		Poly p = PolyZero();
 		Push(s, &p);
+
+	} else if (strcmp(command, "IS_COEFF") == 0) {
+		Poly top = GetTop(s);
+		BoolPrint(PolyIsCoeff(&top));
+
+	} else if (strcmp(command, "IS_ZERO") == 0) {
+		Poly top = GetTop(s);
+		BoolPrint(PolyIsZero(&top));
+
+	} else if (strcmp(command, "CLONE") == 0) {
+		Poly top = GetTop(s);
+		Poly p = PolyClone(&top);
+		Push(s, &p);
+
+	} else if (strcmp(command, "ADD") == 0) {
+		AddTwoPolysFromStack(s, row);
+
+	} else if (strcmp(command, "MUL") == 0) {
+		MultiplyTwoPolysFromStack(s, row);
+
+	} else if (strcmp(command, "NEG") == 0) {
+		Poly p = Pop(s);
+		Poly q = PolyNeg(&p);
+		PolyDestroy(&p);
+		Push(s, &q);
+
+	} else if (strcmp(command, "SUB") == 0) {
+		SubtractTwoPolysFromStack(s, row);
+
+	} else if (strcmp(command, "IS_EQ") == 0) {
+		BoolPrint(AreEqualTwoPolysFromStack(s));
+
+	} else if (strcmp(command, "DEG") == 0) {
+		Poly top = GetTop(s);
+		printf("%d\n", PolyDeg(&top));
+
+	} else if (strcmp(command, "PRINT") == 0) {
+		Poly top = GetTop(s);
+		PolyPrint(&top);
+		printf("\n");
+
+	} else if (strcmp(command, "POP") == 0) {
+		Poly p = Pop(s);
+		PolyDestroy(&p);
+
+	} else if (strncmp(command, "DEG_BY ", 7) == 0) { // TODO ERROR MSGS
+		Poly top = GetTop(s);
+		printf("%d\n", PolyDegBy(&top, strtol(command + 7, NULL, 10)));
+
+	} else if (strncmp(command, "AT ", 3) == 0) { // TODO ERROR MSGS
+		Poly top = GetTop(s);
+		Poly p = PolyAt(&top, strtol(command + 3, NULL, 10));
+		PolyPrint(&p);
+
+	} else {
+		PrintError(WRONG_COMMAND_ERR, row, 0);
 	}
 }
 
+
 int main() {
-	char buf[BUFSIZ];
 	Stack s = StackEmpty();
-	unsigned i = 0;
 
 	// debug
 	FILE *file;
 	file = fopen("/home/tom/Programy/C/wielomiany/test.txt", "r");
 
 	//while (fgets(buf, sizeof buf, stdin)) {
-	while (fgets(buf, sizeof buf, file)) {
-		buf[strcspn(buf, "\r\n")] = 0; // portability..
-		ExecuteCommand(&s, buf, i);
-		i++;
+	int ch;
+	char command[MAX_SIZE_COMMAND];
+
+	unsigned row = 0;
+	unsigned column = 1;
+
+	while ((ch = SeeChar()) != EOF) {
+
+		if (IsLetter(ch)) {
+			fgets(command, MAX_SIZE_COMMAND, stdin);
+			command[strcspn(command, "\r\n")] = 0; // \r for portability
+			ExecuteCommand(&s, command, row);
+
+		} else {
+			Poly p = PolyParseStdin(row, &column);
+			Push(&s, &p);
+		}
+
+		row++;
 	}
 	StackDestroy(&s);
 }
--- src/stack.h
+++ src/stack.h
@@ -9,21 +9,33 @@
 
 typedef struct Stack {
 	size_t element_count;
+	size_t size;
 	Poly* elements;
 } Stack;
 
 static inline Stack StackEmpty() {
 	Stack s;
 	s.element_count = 0;
+	s.size = STACK_SIZE;
 	s.elements = calloc(STACK_SIZE, sizeof(Poly));
 	return s;
 }
 
 static inline void StackDestroy(Stack *s) {
 	free(s->elements);
+	s->element_count = 0;
+	s->size = 0;
+}
+
+static inline void GrowStack(Stack *s) {
+	s->size *= 2;
+	s->elements = (Poly *) realloc(s->elements, s->size * sizeof(Poly));
 }
 
 static inline void Push(Stack *s, Poly *p) {
+	if (s->element_count == s->size) {
+		GrowStack(s);
+	}
 	s->elements[s->element_count] = *p;
 	s->element_count++;
 }
